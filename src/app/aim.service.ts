feeb5757d3fb6da9934cec2693f1028821d694db
Brea Borlas, Julia Sheremet, Matt Miller and William Chu <brea@>
2018-05-14 14:53:13 -0700

    Add getMessagesByUserId function to service to get messages from firebase.

--- src/app/aim.service.ts
+++ src/app/aim.service.ts
@@ -5,12 +5,20 @@ import { UserData } from './models/user-data.model';
 
 @Injectable()
 export class AimService {
+  convos: FirebaseListObservable<any[]>;
+  messages: FirebaseListObservable<any[]>;
 
   constructor(private database: AngularFireDatabase){
   }
 
+  getBuddy(userId: string) {
+    this.convos = this.database.list(`users/${userId}/chatList`);
+    return this.convos;
+  }
+
   getMessagesByUserId(userId: string) {
-    return this.database.list(`users/${userId}/chatList`);
+    this.messages = this.database.list(`users/${userId}/chatList/0/messages`);
+    return this.messages;
   }
 
   appendMessage(newMessage: string, userId: string) {
--- src/app/chat-display/chat-display.component.ts
+++ src/app/chat-display/chat-display.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { AimService } from '../aim.service';
 import { ActivatedRoute, Params } from '@angular/router';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 
 @Component({
@@ -11,25 +12,29 @@ import { ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['./chat-display.component.css'],
   providers: [AimService]
 })
+
 export class ChatDisplayComponent implements OnInit {
   userId: string;
-  conversation;
+  conversation: FirebaseListObservable<any[]>;
+  messages: FirebaseListObservable<any[]>;
 
   constructor(private router: Router, private route: ActivatedRoute, private location: Location, private aimService: AimService) { }
 
   ngOnInit() {
     this.userId = this.route.params['_value']['id'];
-    this.aimService.getMessagesByUserId(this.userId).subscribe(
-      dataLastEmittedFromObserver => {
-      this.conversation = dataLastEmittedFromObserver;
-    }
+    this.conversation = this.aimService.getBuddy(this.userId);
+    this.messages = this.aimService.getMessagesByUserId(this.userId);
+    // .subscribe(
+    //   dataLastEmittedFromObserver => {
+    //   this.conversation = dataLastEmittedFromObserver;
+    // }
       // this.conversation = data.json();
       // this.conversation = Array.of(this.conversation);
-  );
+  // );
   }
 
   sendMessage(newMessage) {
-    this.aimService.appendMessage(`${this.userId.name}: ${newMessage}`, this.userId);
+    this.aimService.appendMessage(newMessage, this.userId);
   }
 
 }
